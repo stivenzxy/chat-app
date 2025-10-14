223105919226e35612dc102a0d9d0e84dc104387
stivenzxy <jesuspereztorres420@gmail.com>
2025-10-13 23:44:55 -0500

    chore: segregation in GetRegisteredUsersService (mapper in another class)

---
+++ chat-server/server-application/src/main/java/com/serverApplication/mappers/UserListMapper.java
@@ -0,0 +1,34 @@
+package com.serverApplication.mappers;
+
+import com.serverApplication.dto.UserPresentationDTO;
+import com.serverDomain.entities.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserListMapper {
+    public static UserPresentationDTO toDTO(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        return new UserPresentationDTO(
+                user.getId(),
+                user.getUsername().value(),
+                user.getEmail().value(),
+                user.getIpAddress(),
+                user.getCreatedAt(),
+                user.getPhotoUrl()
+        );
+    }
+
+    public static List<UserPresentationDTO> toDTOList(List<User> users) {
+        if (users == null || users.isEmpty()) {
+            return List.of();
+        }
+
+        return users.stream()
+                .map(UserListMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+}
--- chat-server/server-application/src/main/java/com/serverApplication/useCases/GetRegisteredUsers.java
+++ chat-server/server-application/src/main/java/com/serverApplication/useCases/GetRegisteredUsers.java
@@ -1,49 +1,22 @@
 package com.serverApplication.useCases;
 
 import com.serverApplication.dto.UserPresentationDTO;
+import com.serverApplication.mappers.UserListMapper;
 import com.serverDomain.entities.User;
 import com.serverDomain.repositories.UserRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GetRegisteredUsers {
-    
+
     private final UserRepository userRepository;
 
     public GetRegisteredUsers(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    
 
     public List<UserPresentationDTO> getAllUsersForPresentation() {
         List<User> domainUsers = userRepository.findAll();
-        return mapToPresentationDTOs(domainUsers);
-    }
-    
-
-    private UserPresentationDTO mapToDTO(User user) {
-        if (user == null) {
-            return null;
-        }
-        
-        return new UserPresentationDTO(
-            user.getId(),
-            user.getUsername().value(),
-            user.getEmail().value(),
-            user.getIpAddress(),
-            user.getCreatedAt(),
-            user.getPhotoUrl()
-        );
-    }
-
-    private List<UserPresentationDTO> mapToPresentationDTOs(List<User> users) {
-        if (users == null) {
-            return List.of();
-        }
-        
-        return users.stream()
-                .map(this::mapToDTO)
-                .collect(Collectors.toList());
+        return UserListMapper.toDTOList(domainUsers);
     }
 }
